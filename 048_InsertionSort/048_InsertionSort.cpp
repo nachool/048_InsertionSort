b4fd0de30eeb755435aa50d81aaebfed09b79ed5
nachool <nachma.olivia1394@gmail.com>
2024-03-14 17:41:06 +0700

    menambahkan prosedur insertion sort

--- 048_InsertionSort/048_InsertionSort.cpp
+++ 048_InsertionSort/048_InsertionSort.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 using namespace std;
 
+int arr[9];
+int n;
+
 void insertionSort() {  //prosedur
-    int i, n, temp, j;
-    int arr[9];
+    int i, temp, j;
+
+    for (i = 1; i <= n; i++) { //step 1
+
+        temp = arr[i]; //step 2
+
+        j = i - 1; //step 3
+
+        while (j >= 0 && arr[j] > temp) //step 4
+        {
+            arr[j + 1] = arr[j];            //step 4 a
+            j--;                            // step 4b
+        }
+        arr[j + 1] = temp;      //step 5
+    }
 }
 
 int main()
